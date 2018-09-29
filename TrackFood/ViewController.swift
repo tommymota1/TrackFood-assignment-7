1a6af2ca5f2ef7d4b6ec226dcd744b861040aa9a
Tommy Mota <thomas.mota@marquette.edu>
2018-09-28 19:05:03 -0500

    Initial Commit

---
+++ TrackFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackFood
+//
+//  Created by Tommy  Mota on 9/28/18.
+//  Copyright © 2018 Mota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
